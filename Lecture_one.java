766073c6fe174515d825f1b167c60af7d49cd0b6
sakshiiii12 <sakshiiii761@gmail.com>
2025-07-07 17:56:40 +0530

    printprogram

--- Lecture_one.java
+++ Lecture_one.java
@@ -88,19 +88,19 @@ public class Lecture_one {
         System.out.println("Area of the circle: " + area);
 
         //3.Make a program that prints the table of a number that is input by the user.
-//        Scanner sc = new Scanner(System.in);
-//        System.out.print("Enter the number:");
-//        int num = sc.nextInt();
-//        System.out.println(num + "x 1 = " + (num * 1));
-//        System.out.println(num + "x 2 = " + (num * 2));
-//        System.out.println(num + "x 3 = " + (num * 3));
-//        System.out.println(num + "x 4 = " + (num * 4));
-//        System.out.println(num + "x 5 = " + (num * 5));
-//        System.out.println(num + "x 6 = " + (num * 6));
-//        System.out.println(num + "x 7 = " + (num * 7));
-//        System.out.println(num + "x 8 = " + (num * 8));
-//        System.out.println(num + "x 9 = " + (num * 9));
-//        System.out.println(num + "x 10 = " + (num * 10));
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter the number:");
+       int num = sc.nextInt();
+       System.out.println(num + "x 1 = " + (num * 1));
+       System.out.println(num + "x 2 = " + (num * 2));
+       System.out.println(num + "x 3 = " + (num * 3));
+       System.out.println(num + "x 4 = " + (num * 4));
+       System.out.println(num + "x 5 = " + (num * 5));
+       System.out.println(num + "x 6 = " + (num * 6));
+       System.out.println(num + "x 7 = " + (num * 7));
+       System.out.println(num + "x 8 = " + (num * 8));
+       System.out.println(num + "x 9 = " + (num * 9));
+       System.out.println(num + "x 10 = " + (num * 10));
 
     }
 }
